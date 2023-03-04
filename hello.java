c423d1206f6c8f3a78f711948a7384563701e2f5
satyam112245 <satyamgupta112245@gamil.com>
2023-03-04 12:30:47 +0700

    Added Hello Program

---
+++ hello.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class hello
+{
+	public static void main(String args[])
+	{
+		System.out.println("hello welcome to syit");
+		System.out.println("This is set pract");
+	}
+}
